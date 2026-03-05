1460d65d231af5aff2c350b06e83b2e068cb2e8e
StudioLee-cmd <tim@studiolee.nl>
2026-03-05 01:03:05 +0100

    Fix siteDetails: niche field, live siteUrl, siteName, language nl-NL; FreeTrialModal uses niche

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,11 +1,12 @@
 export const siteDetails = {
     siteName: 'KliniekAI',
-    siteUrl: 'https://kliniekai.nl/',
+    niche: 'Klinieken',
+    siteUrl: 'https://www.kliniekai.nl/',
     metadata: {
         title: 'KliniekAI - De slimme assistent voor jouw medische kliniek',
         description: 'Bespaar zeeën van tijd en focus op patiëntenzorg. KliniekAI regelt je afspraken, telefoon en administratie volledig automatisch.',
     },
-    language: 'nl-nl',
+    language: 'nl-NL',
     locale: 'nl-NL',
     siteLogo: '/images/logo-transparent.png',
     googleAnalyticsId: '', // e.g. G-XXXXXXX,
